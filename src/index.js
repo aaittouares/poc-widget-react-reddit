8cd824871ecd85439eed4bcdf73d5e578d792e8a
alexandre aittouares <alex.aittouares@gmail.com>
2022-05-19 20:59:37 +0200

    Modify config for parcel and remove webvitals

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 
 // Fonction callback à éxécuter quand une mutation est observée
 const callback = function (mutationsList) {
@@ -53,7 +51,3 @@ global.reddit_widget = {init: function(el){
 
   observer.observe(el, config);
 }};
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
